db180f918f51d764f447d0c7061798716967ee02
felixjb <felixjbcomp@gmail.com>
2026-05-17 19:03:14 +0200

    chore: remove stray test dir

--- test/greet.node.ts
+++
@@ -1,9 +0,0 @@
-import assert from "node:assert";
-import { describe, it } from "node:test";
-import { greet } from "../src/greet.ts";
-
-describe("greet", () => {
-  it("should return a greeting for the given name", () => {
-    assert.strictEqual(greet("World"), "Hello, World!");
-  });
-});
